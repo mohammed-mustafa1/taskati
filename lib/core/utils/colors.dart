42e448e2bd7712026b7a36ef02b8338dca2035d5
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2025-05-17 06:06:50 +0300

    add AppColors class

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff4E5AE8);
+  static const Color orange = Color(0xFFFF8746);
+  static const Color red = Color(0xFFFF4667);
+  static const Color black = Color(0xFF121212);
+  static const Color grey = Color(0xffB4AAAA);
+}
