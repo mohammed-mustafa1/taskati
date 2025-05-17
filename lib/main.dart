48be7964b75bbfaa78fff71b666ad5cb86a8f85b
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2025-05-17 05:48:45 +0300

    create project structure

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class Taskati extends StatelessWidget {
+  const Taskati({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
